1adcbb82b198841b639e026bbf294e7b81beaaa0
ThreeDokNait <nikitaaleksandrovichshiyatov@gmail.com>
2023-08-04 16:15:30 +0300

    Коммит для пуша

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿Console.Clear();
+
+// Task 1
+// Console.WriteLine("Enter the coordinates X, please");
+// int x = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Enter the coordinates Y, please");
+// int y = Convert.ToInt32(Console.ReadLine());
+// if (x>0 && y>0)
+// {
+//     Console.WriteLine("Firstquarter");
+// }
+// else if (x>0 &&y<0)
+// {
+//     Console.WriteLine("Secondquarter");
+// }
+// else if (x>0 &&y>0)
+// {
+//     Console.WriteLine("Thirdquarter");
+// }
+// else if (x<0 &&y>0)
+// {
+//     Console.WriteLine("Fourthquarter");
+// }
+// else
+// {
+//     Console.WriteLine("Not found");
+// }
+
+// Task 2
+
+// Console.WriteLine("Imput number");
+// double firstquarter = Convert.ToInt32(Console.ReadLine());
+
+// if (firstquarter == 1)
+// {
+//     Console.WriteLine("x>0, y>0");
+// }
+// else if (firstquarter == 2)
+// {
+//     Console.WriteLine("X>0, Y<0");
+// }
+// else if (firstquarter == 3)
+// {
+//     Console.WriteLine("X<0, Y<0");
+// }
+// else if (firstquarter == 4)
+// {
+//     Console.WriteLine("X<0, Y>0");
+// }
+// else
+// {
+//     Console.WriteLine("You're liar! There is no such value here! Get nimber 1-4");
+// }
+
+// Task 3
+
+// Console.WriteLine("Imput number X1");
+// double firstX = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Imput number Y");
+// double firstY = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Imput number X2");
+// double secondX = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Imput number Y2");
+// double secondY = Convert.ToInt32(Console.ReadLine());
+
+// double number = Math.Sqrt(Math.Pow(secondX-firstX, 2) + Math.Pow(secondY-firstY,2));
+// Console.WriteLine(number);
+
+
+// Task 4
+
+// Console.WriteLine("get number for me");
+// double number = Convert.ToDouble(Console.ReadLine());
+// for (int i = 1; i <= number; i++)
+// {
+//     Console.WriteLine(Math.Pow (i, 2));
+// }
